26f35ffab8d8d026a2fbb1c023bf36ecb04b96bb
zadpo <2htrading2022@gmail.com>
2025-03-08 09:43:32 +0800

    fix header

--- src/providers/redux-provider.tsx
+++ src/providers/redux-provider.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { Footer } from "@/components/footer";
-import { Navbar } from "@/components/navbar";
 import { store } from "@/lib/store";
 import { Provider } from "react-redux";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider store={store}>
-      <Navbar />
-      {children}
-      <Footer />
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,13 +1,17 @@
+import { Navbar } from "@/components/navbar";
 import { Benefits, CTABanner, Features, HeroSection, ServicesSection } from "./HomePage";
+import { Footer } from "@/components/footer";
 
 export function HomePage() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <Benefits />
       <ServicesSection />
       <Features />
       <CTABanner />
+      <Footer />
     </>
   );
 }
